4f2b133c3c0c8dedf8c37f93e9b0eceee2c37f0c
Alejahndro <alejahndro02@gmail.com>
2021-04-29 12:10:45 -0500

    Se declararon las variables correspondientes a nombre, fecha, hora y sintomas, tambien se crearon los metodos de agendarCita y resetCampos

--- src/app/components/crear-citas/crear-citas.component.ts
+++ src/app/components/crear-citas/crear-citas.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-citas.component.css']
 })
 export class CrearCitasComponent implements OnInit {
+  nombre: '';
+  fecha:'';
+  hora:'';
+  sintomas:'';
+  incorrecto = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  agengarCita():void{
+    if(this.nombre=='' || this.fecha=='' || this.hora=='' || 
+      this.sintomas==''){
+        this.incorrecto = true;
+        return;
+      }
+      this.incorrecto = false;
 
+
+      const CITA = {
+        nombre: this.nombre,
+        fecha: this.fecha,
+        hora: this.hora,
+        sintomas: this.sintomas
+      }
+      this.resetCampos();
+
+  }
+  resetCampos(){
+    this.nombre = '';
+    this.fecha = '';
+    this.hora = '';
+    this.sintomas = '';
+  }
 }
