10caaea21db1e849b6a1b4e5bf5f3c753860f53d
Alejahndro <alejahndro02@gmail.com>
2021-04-29 13:08:57 -0500

    Se creo la variable listCitas de tipo array y se creo el metodo agendarCita para poder guardar los datos en el array antes creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'citas';
+  listCitas: any[] = [];
+
+  agendarCita(cita: any){
+     this.listCitas.push(cita)
+  }
 }
